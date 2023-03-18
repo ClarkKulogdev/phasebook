8bad85aa070da4c5ab776a7b6dc8fe2f46e6b2f1
Clark Dalugdog <47905400+ClarkKulogdev@users.noreply.github.com>
2023-03-18 15:10:10 +0800

    Update __init__.py
    add renter_template('index.html')

--- phasebook/__init__.py
+++ phasebook/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 
 from . import match, search
 
@@ -7,10 +7,14 @@ def create_app():
     app = Flask(__name__)
 
     @app.route("/")
-    def hello():
-        return "Hello World!"
-
+    # def hello():
+    #     return "Hello World!"
+    
+    def index():
+        return render_template('index.html')
+    
     app.register_blueprint(match.bp)
     app.register_blueprint(search.bp)
+
 
     return app
